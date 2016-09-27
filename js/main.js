e9dee2f3c315a527fff3650a9b6a33a95574609a
Jason Gresalfi <jason@Jasons-MacBook-Air.local>
2016-09-27 15:10:10 -0500

    Added flex styles, and hooked up ajax request to quote API and tested.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 'use strict';
 
 $(document).ready(function() {
-	$("#btn").on("click", function() {
-		$.getJSON("https://got-quotes.herokuapp.com/quotes", function(data) {
-			$(".quote").html(data.quote);
-			console.log("AJAX call: successful")
-		})
-	})
+    $("#btn").on("click", function() {
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://andruxnet-random-famous-quotes.p.mashape.com/",
+            "method": "POST",
+            "headers": {
+                "cache-control": "no-cache",
+                "postman-token": "0bfbd25b-5b2a-60be-bfe5-efe940d10583",
+                "X-Mashape-Key": "9zwFYYC4i1msh2sU64WFOsp524C9p1woBdkjsnZwD7WKQR3vzj",
+                "Content-type": "application/json; charset=utf-8",
+                "dataType": "json",
+                "data": "data",
+                'Accept': 'application/json'
+            }
+        }
+        $.getJSON(settings).done(function(response) {
+            console.log(response.quote);
+            $(".quote").html("<h2>" + response.quote + "</h2>");
+            $(".author").html("<h3>" + " - " + response.author + "</h3>");
+        });
+    })
 })
